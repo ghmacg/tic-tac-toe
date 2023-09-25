187737368cd6ace4cccf76bab9ca0df837635768
miguel <91511269+ghmacg@users.noreply.github.com>
2023-09-25 15:56:28 -0400

    Create factory function for game logic - Create method to switch active player - Create method to get active player - Create method to play round without the logic for the winner

--- scripts/main.js
+++ scripts/main.js
@@ -63,3 +63,46 @@ const Gameboard = () => {
 };
 
 
+// Logic of the game
+const GameController = (
+    // If no names assigned these are the default names
+    playerOneName = 'Player One',
+    playerTwoName = 'Player Two'
+) => {
+    // Initialize Gameboard and Players objects
+    const board = Gameboard();
+    const playerOne = Player(playerOneName, 'X');
+    const playerTwo = Player(playerTwoName, 'O');
+
+    const players = [playerOne, playerTwo];
+    let activePlayer = players[0];
+
+    // Used to switch the active player when current player finishes his turn  
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    const playRound = (row, column) => {
+        // First check if the value of the selected cell is already played, 
+        // if it is stop the move
+        if (board.getBoard()[row][column].getValue() !== '') return;
+        
+        // Make the selected cell take the value of players token
+        board.dropToken(row, column, getActivePlayer().getToken());
+
+        /*  This is where we would check for a winner and handle that logic,
+        such as a win message. */
+
+        switchPlayerTurn();
+    };
+
+    return {
+        playRound,
+        getActivePlayer,
+        getBoard: board.getBoard
+    };
+};
+
+
