fe8ac6fe437946c247220fb44ac379ea43280403
miguel <91511269+ghmacg@users.noreply.github.com>
2023-09-25 15:54:06 -0400

    Create factory function for the gameboard

--- scripts/main.js
+++ scripts/main.js
@@ -32,3 +32,34 @@ const Cell = () => {
 };
 
 
+// Logic of the gameboard
+const Gameboard = () => {
+    // Define varibles for the area of gameboard,
+    // the area is going to be defined in board array
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    // Push into board 3 arrays with 3 cell spaces in each,
+    // to simulate the area we want for the game
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        };
+    };
+
+    const getBoard = () => board; 
+
+    // Assigns the token value of the player to the selected cell 
+    const dropToken = (selectedRow, selectedColumn, player) => {
+        board[selectedRow][selectedColumn].addToken(player);
+    };
+
+    return {
+        getBoard, 
+        dropToken, 
+    };
+};
+
+
